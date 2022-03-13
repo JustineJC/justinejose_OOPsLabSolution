5c2ad7d7ba3659cdb09b601a2b014e15cf01aab0
JustineJC <99407404+JustineJC@users.noreply.github.com>
2022-03-13 12:23:02 +0400

    updated file

--- Employee.java
+++ Employee.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.random.RandomGenerator;
-import java.lang.Runtime.*;
+import java.lang.*;
 public class Employee{
    public String firstName,lastName;
     Employee(String firstName,String lastName){
@@ -11,18 +11,26 @@ public class Employee{
     }
 
     public void generatePassword(){
-        String a = "abcdefghijklmnopqrst";
-        System.out.println(Random(a)); 
+        char[] stringl = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+        StringBuilder random =  new StringBuilder();
+        int length=11;
+        for(int i =0;i<length;i++){
+            int index = (int) (Math.random()*stringl.length); 
+            random.append(stringl[index]);
+        } System.out.println(random.toString());
     }
 
     public void generateEmailAddress(String department){
         System.out.println(firstName+lastName+"@"+department+".greatlearning.com");
     }
 
-    public void showCredentials(){
-        System.out.println("First Name: "+firstName);
-        System.out.println("Last Name: "+lastName);
+    public void showCredentials(String department,String firstName){
+        System.out.println("Dear "+firstName+" your generated credentials are follows");
+        System.out.println("Email ---> ");
+        generateEmailAddress(department);
+        generatePassword();
         
+
     }
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
@@ -41,19 +49,19 @@ public static void main(String[] args) {
                 break;
             case 1:
                 Employee obj = new Employee("harshit","choudary");
-                obj.generateEmailAddress("Technical");
+                obj.showCredentials("tech", "harshit");
                 break;
             case 2:
                 Employee obj1 =new Employee("John", "Hash");
-                obj1.generateEmailAddress("Admin");
+                obj1.showCredentials("admin", "John");
                 break;
             case 3:
                 Employee obj2 = new Employee("Ram", "Jain");
-                obj2.generateEmailAddress("hr");
+                obj2.showCredentials("hr", "Ram");;
                 break;
             case 4:
                 Employee obj3 = new Employee("Srihari", "krishna"); 
-                obj3.generateEmailAddress("Legal");
+                obj3.showCredentials("legal", "Srihari");
                 break;
             default: 
                 System.out.println("\nPlease enter a valid input:");    
